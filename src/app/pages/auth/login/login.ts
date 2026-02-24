a78855bfa9116e50ce05820d3dfff7b2a4b1cfce
pao1605 <paoladianamh05@gmail.com>
2026-02-23 23:00:42 -0600

    Practicaa 3

--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,17 +1,75 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
+    ReactiveFormsModule,
     ButtonModule,
     CardModule,
-    InputTextModule
+    InputTextModule,
+    PasswordModule,
+    ToastModule
   ],
-  templateUrl: './login.html',
-  styleUrls: ['./login.scss']
+  providers: [MessageService],
+  templateUrl: './login.html'
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  loginForm!: FormGroup;
+
+  
+  private USER = 'Pao';
+  private PASS = 'Pao@162005';
+
+  constructor(
+    private fb: FormBuilder,
+    private message: MessageService
+  ) {
+
+    this.loginForm = this.fb.group({
+      user: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  login() {
+
+    
+    if (this.loginForm.invalid) {
+      this.message.add({
+        severity: 'warn',
+        summary: 'Campos incompletos',
+        detail: 'Debes completar todos los campos'
+      });
+      return;
+    }
+
+    const { user, password } = this.loginForm.value;
+
+    
+    if (user === this.USER && password === this.PASS) {
+      this.message.add({
+        severity: 'success',
+        summary: 'Login correcto',
+        detail: 'Bienvenido al sistema'
+      });
+    } 
+   
+    else {
+      this.message.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Usuario o contraseña incorrectos'
+      });
+    }
+  }
+}
--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -1,17 +1,81 @@
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [
+    CommonModule,          
+    ReactiveFormsModule,
     ButtonModule,
     CardModule,
-    InputTextModule
+    InputTextModule,
+    PasswordModule,
+    ToastModule
   ],
-  templateUrl: './register.html',
-  styleUrls: ['./register.scss']
+  providers: [MessageService],
+  templateUrl: './register.html'
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+
+  registerForm!: FormGroup; 
+
+  constructor(
+    private fb: FormBuilder,
+    private message: MessageService
+  ) {
+    this.registerForm = this.fb.group({
+      user: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        Validators.pattern(/^(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/)
+      ]],
+      confirmPassword: ['', Validators.required],
+      fullName: ['', Validators.required],
+      address: ['', Validators.required],
+      age: [null, [Validators.required, Validators.min(18)]],
+      phone: ['', [
+        Validators.required,
+        Validators.pattern(/^[0-9]{10}$/)
+      ]]
+    });
+  }
+
+  register() {
+
+    if (this.registerForm.invalid) {
+      this.message.add({
+        severity: 'error',
+        summary: 'Formulario inválido',
+        detail: 'Completa todos los campos correctamente'
+      });
+      return;
+    }
+
+    if (this.registerForm.value.password !== this.registerForm.value.confirmPassword) {
+      this.message.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Las contraseñas no coinciden'
+      });
+      return;
+    }
+
+    this.message.add({
+      severity: 'success',
+      summary: 'Registro exitoso',
+      detail: 'Usuario registrado correctamente'
+    });
+  }
+}
+
